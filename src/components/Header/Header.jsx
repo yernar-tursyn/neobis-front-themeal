1c17160da7000415953da9cd9d178fad540b0d7c
Yernar Tursyn <tursynyernar@gmail.com>
2024-06-08 15:26:45 +0500

    feat: made a transition between pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
 
 export const Header = () => {
   return (
     <div>
-        <header class="header"><h1>The Meal</h1></header>
+      <header className="header">
+        <Link to="/" className="header-link">
+          <h1>The Meal</h1>
+        </Link>
+      </header>
     </div>
-  )
-}
+  );
+};
--- src/components/Instruction/Instruction.jsx
+++ src/components/Instruction/Instruction.jsx
@@ -3,38 +3,36 @@ import './instruction.css';
 
 export const Instruction = () => {
   return (
-    <div>
-      <div className="meal-detail">
-        <section className="meal-detail-base-info-block">
-          <div className="meal-detail-base-info-text">
-            <p className="title">Banana Pancakes</p>
-            <p className="meal-detail-base-info">Dessert | American</p>
-            <div className="ingredients">
-              <p>- Banana <b>1 large</b></p>
-              <p>- Eggs <b>2 medium</b></p>
-              <p>- Baking Powder <b>pinch</b></p>
-              <p>- Vanilla Extract <b>sprinkling</b></p>
-              <p>- Oil <b>1 tsp</b></p>
-              <p>- Pecan Nuts <b>25g</b></p>
-              <p>- Raspberries <b>125g</b></p>
-            </div>
+    <div className="meal-detail">
+      <section className="meal-detail-base-info-block">
+        <div className="meal-detail-base-info-text">
+          <p className="title">Banana Pancakes</p>
+          <p className="meal-detail-base-info">Dessert | American</p>
+          <div className="ingredients">
+            <p>- Banana <b>1 large</b></p>
+            <p>- Eggs <b>2 medium</b></p>
+            <p>- Baking Powder <b>pinch</b></p>
+            <p>- Vanilla Extract <b>sprinkling</b></p>
+            <p>- Oil <b>1 tsp</b></p>
+            <p>- Pecan Nuts <b>25g</b></p>
+            <p>- Raspberries <b>125g</b></p>
           </div>
-          <div className="meal-detail-base-info-img">
-            <img 
-              src="https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg" 
-              alt="Banana Pancakes" 
-            />
-          </div>
-        </section>
-        <section className="meal-detail-instruction">
-          <h2 className="h2">Instruction</h2>
-          <pre>
+        </div>
+        <div className="meal-detail-base-info-img">
+          <img 
+            src="https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg" 
+            alt="Banana Pancakes" 
+          />
+        </div>
+      </section>
+      <section className="meal-detail-instruction">
+        <h2 className="h2">Instruction</h2>
+        <pre>
 {`In a bowl, mash the banana with a fork until it resembles a thick purée. Stir in the eggs, baking powder and vanilla.
 Heat a large non-stick frying pan or pancake pan over a medium heat and brush with half the oil. Using half the batter, spoon two pancakes into the pan, cook for 1-2 mins each side, then tip onto a plate. Repeat the process with the remaining oil and batter. Top the pancakes with the pecans and raspberries.`}
-          </pre>
-          <a className="meal-detail-youtube" href="https://www.youtube.com/watch?v=kSKtb2Sv-_U">Watch on YouTube</a>
-        </section>
-      </div>
+        </pre>
+        <a className="meal-detail-youtube" href="https://www.youtube.com/watch?v=kSKtb2Sv-_U">Watch on YouTube</a>
+      </section>
     </div>
   );
 };
